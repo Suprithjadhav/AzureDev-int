ca9370c8fb90984e1752526f338284d074720e7b
SuprithJadhav <suprithjadhav007@gmail.com>
2025-06-09 18:41:29 +0530

    new commit

---
+++ TestProject1/Script/Unit2.js
@@ -0,0 +1,11 @@
+﻿function Test1()
+{
+  var Var1;
+  Var1 = 1234;
+  //Launches the specified browser and opens the specified URL in it.
+  Browsers.Item(btEdge).Run("https://support.smartbear.com/testcomplete/docs/working-with/integration/azure/test-adapter/requirements.html");
+  //Simulates a left-button single click in a window or control as specified (relative position, shift keys).
+  Aliases.browser.pageIntegrationWithAzureDevopsAn.textnodeTestcompleteDocumentatio.textnodeGeneralInformation.linkGeneralInformation.Click();
+  //Posts an information message to the test log.
+  Log.Message(Var1, "");
+}
